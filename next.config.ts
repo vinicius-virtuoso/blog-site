e3ece5b58b76a9433cf552ef1c5e17e2b77dae4b
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2025-11-15 13:48:15 -0300

    contentlayer configurado

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
+import { withContentlayer } from "next-contentlayer";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  turbopack:{}
 };
 
-export default nextConfig;
+export default withContentlayer(nextConfig)
+
+
+
